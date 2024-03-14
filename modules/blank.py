182871b3ba19265077a462334796fb82e2da52a2
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2024-03-14 14:56:03 +0300

    blank.py

--- modules/blank.py
+++ modules/blank.py
@@ -7,6 +7,7 @@ def blank_auto(cvector, cinfo, developer):
     i_for_blank['heat_exchanger'] = cinfo[0][1]
     i_for_blank['temperature'] = cinfo[1][1]
     i_for_blank['consumption'] = cinfo[2][1]
+    i_for_blank['consumption_from_blank'] = cinfo[-1][1]
     i_for_blank['glycol'] = cinfo[3][1]
     i_for_blank['orderer'] = cinfo[4][1]
     i_for_blank['object'] = cinfo[5][1] if len(cinfo[5][1])<40 else '\n'.join([' '.join(cinfo[5][1].split()[:3]), ' '.join(cinfo[5][1].split()[3:])]) if len(cinfo[5][1])<80 else '\n'.join([' '.join(cinfo[5][1].split()[:3]), ' '.join(cinfo[5][1].split()[3:6]), ' '.join(cinfo[5][1].split()[6:])])
